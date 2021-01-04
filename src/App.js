852f3a27e2b309cd2e0baf256abf8b1824c1ae5d
th7024 <th7024@gmail.com>
2021-01-04 22:33:49 +0900

    컵포넌트에 정보추가해서 사용하기props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Potato from './Potato';
+
+
+function Food({fav}) {   //기본형태 (props) props,fav == ({fav}) 
+  return <h3>I love {fav}</h3>
+}
 
 function App() {
   return (
   <div>
     <h1>Hello!</h1>
-    <Potato/>
+    <Food fav="kimchi"/>
+    <Food fav="라면"/>
+    <Food fav="만두"/>
   </div>
     );
 }
