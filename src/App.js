88d438eabd5a2f4ff6617280361fbdde68e9b9df
th7024 <th7024@gmail.com>
2021-01-08 21:58:25 +0900

    class 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,69 +1,27 @@
 import React from 'react';
 import propTypes from "prop-types";
 
-function Food({ name, picture, rating }) {  //기본형태 (props) props,fav == ({fav}) 
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-Food.propTypes = {
-  name: propTypes.string.isRequired, //proptype에는 string.isRequired가 꼭 필요 isRequired는 무조건 있어야 하는 내용
-  picture: propTypes.string.isRequired,
-  rating: propTypes.number.isRequired // rating은 int라서 number
-};
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 5.0
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-      rating: 4.9
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-      rating: 4.8
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-      rating: 5.5
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-      rating: 4.7
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  add =() => {
+    console.log("add");
+  };
+  minus =() => {
+    console.log("minus");
+  };
+  render(){
+    return (
+    <div>
+      <h1> The number is: {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+      </div>
+      );
   }
-];
 
+}
 
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
-    </div>
-  );
-
-      }
 export default App;
