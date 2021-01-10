1e7c4cfa362c30ad61582c46fd57bc0c69e8774c
th7024 <th7024@gmail.com>
2021-01-10 23:52:12 +0900

    컴포넌트 라이프 사이클

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import propTypes from "prop-types";
+
 
 
 class App extends React.Component{
+  constructor(props){ //자바스크립트에서 class를 만들 때 호출되는 것, mounting안에 있는 것으로서 render보다 먼저 실행됨
+    super(props);
+console.log("내가 제일 빠름")
+  }
   state = {
     count: 0
   };
@@ -12,7 +16,17 @@ class App extends React.Component{
   minus =() => {
     this.setState(current => ({count: current.count - 1}));
   };
-  render(){
+  componentDidMount(){ 
+    console.log("랜더링됐다.")
+  }
+  componentDidUpdate(){//update는 react가 실행되는 것을 말함.버튼을 누르면 업데이트가 됨
+    console.log("업데이트됨.")
+  }
+  componentWillUnmount(){
+    console.log("나는 떠난다.") //컴포넌트가 떠날 때 호출 
+  }
+  render(){ //순서 : constructor - render - componentDidMount
+    console.log("랜더링 중")
     return (
     <div>
       <h1> The number is: {this.state.count}</h1>
