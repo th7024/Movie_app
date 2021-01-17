df85261fd7dd6c1ea55d84759764ba79dc81b12e
th7024 <th7024@gmail.com>
2021-01-17 22:41:51 +0900

    컴포넌트 클래스 화

--- src/App.js
+++ src/App.js
@@ -20,18 +20,28 @@ class App extends React.Component{
   }
   render(){
     const {isLoading, movies} = this.state;
-    return <div>{isLoading ? "Loading...": movies.map(movie =>{
-      console.log(movie);
-      return <Movie
-      key={movie.id}
-      id={movie.id}
-      year={movie.year}
-      title={movie.title}
-      summary={movie.summary}
-      poster={movie.poster}/>;
-
-    })}</div>
-     
+    return( 
+      <section class="container">
+      {isLoading ? (
+        <div class="loader">
+          <span class="loader__text">Loading...</span>
+        </div>
+      ) : (
+        <div class="movies">
+          {movies.map(movie => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+            />
+          ))}
+    </div>
+      )}
+    </section>
+    );
   }
 
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 //컴포넌트가 state가 필요하지 않은 경우 : function의 형태로 할 수 있음.
-function Movie({id,year,title,summary,poster}){
-return <h5>{title}</h5>;
+function Movie({ year, title, summary, poster }) {
+    return (
+      <div class="movie">
+        <img src={poster} alt={title} title={title} />
+        <div class="movie__data">
+          <h3 class="movie__title">{title}</h3>
+          <h5 class="movie__year">{year}</h5>
+          <p class="movie__summary">{summary}</p>
+        </div>
+      </div>
+    );
 }
 
 Movie.propTypes ={
-    id: propTypes.number.isRequired,
-    year: propTypes.number.isRequired,
-    title: propTypes.string.isRequired,
-    summary: propTypes.string.isRequired,
-    poster: propTypes.string.isRequired
-};
+    id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+  };
 
 export default Movie;
