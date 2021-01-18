693397379303400e002955fb739d8c811d04c39c
th7024 <th7024@gmail.com>
2021-01-18 22:00:00 +0900

    map을 이용해 genres 사용

--- src/Movie.js
+++ src/Movie.js
@@ -2,14 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 //컴포넌트가 state가 필요하지 않은 경우 : function의 형태로 할 수 있음.
-function Movie({ year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
     return (
-      <div class="movie">
+      <div className="movie">
         <img src={poster} alt={title} title={title} />
-        <div class="movie__data">
-          <h3 class="movie__title">{title}</h3>
-          <h5 class="movie__year">{year}</h5>
-          <p class="movie__summary">{summary}</p>
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="genres">
+            {genres.map((genre, index) => (
+            <li key={index} className="genres_genre">{genre}</li>
+            ))}</ul>
+          <p className="movie__summary">{summary}</p>
         </div>
       </div>
     );
@@ -20,7 +24,8 @@ Movie.propTypes ={
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
   };
 
 export default Movie;
